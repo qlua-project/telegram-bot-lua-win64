f36f4f26cb732c03351a6f6963b262f2a1a7cda5
ivansoft <32499694+ivansoft@users.noreply.github.com>
2025-03-23 09:16:16 +0300

    lua5*.bat LUAROCKS_SYSCONFDIR revert

--- lua53.bat
+++ lua53.bat
@@ -6,8 +6,8 @@ set "CWD=%BATCH_DIR:~0,-1%"
 set "CWD_LUA=%CWD:\=\\%"
 set "APPDATA_LUA=%APPDATA:\=\\%"
 
-rem set "LUAROCKS_SYSCONFDIR=C:\Program Files\luarocks"
-set "LUAROCKS_SYSCONFDIR=%CWD%\.luarocks"
+set "LUAROCKS_SYSCONFDIR=C:\Program Files\luarocks"
+rem set "LUAROCKS_SYSCONFDIR=%CWD%\.luarocks"
 
 "externals/lua-5.3.5\bin\lua.exe" -e "package.path=\"%CWD_LUA%\\lua_modules\\share\\lua\\5.3\\?.lua;%CWD_LUA%\\lua_modules\\share\\lua\\5.3\\?\\init.lua;%APPDATA_LUA%\\luarocks\\share\\lua\\5.3\\?.lua;%APPDATA_LUA%\\luarocks\\share\\lua\\5.3\\?\\init.lua;\"..package.path;package.cpath=\"%CWD_LUA%\\lua_modules\\lib\\lua\\5.3\\?.dll;%APPDATA_LUA%\\luarocks\\lib\\lua\\5.3\\?.dll;\"..package.cpath" %I% %*
 exit /b %ERRORLEVEL%
