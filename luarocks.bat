5793f27c15b95b4bc9057193fdbe9617e72449d7
ivansoft <32499694+ivansoft@users.noreply.github.com>
2025-03-23 04:02:58 +0300

    luarocks VS BuildTools 2022 (x86_64-w64-mingw32-gcc by default)
    x86_64-w64-mingw32-gcc -O2 -c -o lpcap.o -I%~dp0externals\lua-5.4.1/include lpcap.c
    'x86_64-w64-mingw32-gcc' is not recognized as an internal or external command,operable program or batch file.

--- luarocks.bat
+++ luarocks.bat
@@ -6,5 +6,7 @@ set "CWD=%BATCH_DIR:~0,-1%"
 set "LUAROCKS_SYSCONFDIR=C:\Program Files\luarocks"
 rem set "LUAROCKS_SYSCONFDIR=%CWD%\.luarocks"
 
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64 > NUL
+
 "%~dp0externals\luarocks.exe" --project-tree %~dp0lua_modules %*
 exit /b %ERRORLEVEL%
