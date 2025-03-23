ead1fde6eee7b247a7729376a631416ddf33d3ba
ivansoft <32499694+ivansoft@users.noreply.github.com>
2025-03-23 09:12:30 +0300

    luarocks.bat absolute OPENSSL_DIR

--- luarocks.bat
+++ luarocks.bat
@@ -9,5 +9,5 @@ rem set "OPENSSL_DIR=%~dp0externals\openssl"
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64 > NUL
 
-"%~dp0externals\luarocks.exe" --project-tree %~dp0lua_modules %*
+"%~dp0externals\luarocks.exe" --project-tree %~dp0lua_modules "OPENSSL_DIR=%CD%\externals\openssl" %*
 exit /b %ERRORLEVEL%
