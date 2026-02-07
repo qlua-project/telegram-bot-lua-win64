5b63b50db4c2fd000025e16f4732aa3e72642cee
ivansoft <32499694+ivansoft@users.noreply.github.com>
2026-02-07 08:10:43 +0300

    unused .luarocks configs

--- .luarocks/config-5.3.lua
+++
@@ -1,7 +0,0 @@
-variables = {
-   LUA = "externals/lua-5.3.5\\bin\\lua.exe",
-   LUA_BINDIR = "externals/lua-5.3.5\\bin",
-   LUA_DIR = "externals/lua-5.3.5",
-   LUA_INCDIR = "externals/lua-5.3.5/include",
-   OPENSSL_DIR= "externals/openssl",
-}
--- .luarocks/config-5.4.lua
+++
@@ -1,7 +0,0 @@
-variables = {
-   LUA = "externals/lua-5.4.1\\bin\\lua.exe",
-   LUA_BINDIR = "externals/lua-5.4.1\\bin",
-   LUA_DIR = "externals/lua-5.4.1",
-   LUA_INCDIR = "externals/lua-5.4.1/include",
-   OPENSSL_DIR= "externals/openssl",
-}
